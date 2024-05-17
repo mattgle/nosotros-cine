48665ea288f58f03c82a4110f82b16eae18004e8
Manu Oreguy <manuoreguy@gmail.com>
2024-05-17 14:50:18 -0300

    Update src/constants/urls.tsx
    Co-authored-by: mattgle <44204622+mattgle@users.noreply.github.com>

--- src/constants/urls.tsx
+++ src/constants/urls.tsx
@@ -1,4 +1,4 @@
 // Add App URLs here.
-export const INSTAGRAM_URL = 'https://www.instagram.com/[TODO_INGRESAR_IG]/';
+export const INSTAGRAM_URL = 'https://www.instagram.com/nosotroscine/';
 export const EMAIL_URL = '[TODO_INGRESAR_EMAIL]';
 export const WHATSAPP_URL = 'https://wa.me/[TODO_Nro_Whatsapp]';
