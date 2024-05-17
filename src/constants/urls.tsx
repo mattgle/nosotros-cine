6f6075b537bc7da00fa5a8c2190f865e99f0b281
Manu Oreguy <manuoreguy@gmail.com>
2024-05-17 14:50:31 -0300

    Update src/constants/urls.tsx
    Co-authored-by: mattgle <44204622+mattgle@users.noreply.github.com>

--- src/constants/urls.tsx
+++ src/constants/urls.tsx
@@ -1,4 +1,4 @@
 // Add App URLs here.
 export const INSTAGRAM_URL = 'https://www.instagram.com/nosotroscine/';
-export const EMAIL_URL = '[TODO_INGRESAR_EMAIL]';
+export const EMAIL_URL = 'ellocodelasseries23@gmail.com';
 export const WHATSAPP_URL = 'https://wa.me/[TODO_Nro_Whatsapp]';
